30dda47966fe4a8c0a6c94825dca1c01684bb852
Luís Garcês <luis_garces@live.com.pt>
2021-01-24 19:42:23 +0000

    [FIX] Fix build issues with coverlet code coverage (#5)
    * [FIX] Fixing coverage collection and merging scripts.
    
    * [FIX] Fixed results directory.

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,4 +1,4 @@
-﻿$globalTools = dotnet tool list -g
+$globalTools = dotnet tool list -g
 $reportGeneratorTool = $globalTools | Select-String -Pattern "dotnet-reportgenerator-globaltool"
 
 If (!$reportGeneratorTool) {
@@ -9,9 +9,11 @@ $reportTimestamp = [DateTime]::UtcNow.ToString("yyyyMMdd_hhmmss")
 $currentDir = (Get-Location).Path
 $coverageDir = "$currentDir\\coverage-outputs\\$reportTimestamp\\"
 
-dotnet test .\rules-framework.sln /p:CollectCoverage=true /p:CoverletOutputFormat="opencover%2cjson" /p:CoverletOutput=$coverageDir /p:MergeWith="$coverageDir/coverage.json" -m:1
+dotnet test .\rules-framework.sln --collect:"XPlat Code Coverage" --results-directory:"$coverageDir" -m:1
 
-$coverageFile = "$($coverageDir)coverage.opencover.xml"
+reportgenerator -reports:"$($coverageDir)*\\*.xml" -targetdir:"$($coverageDir)" -reporttypes:Cobertura
+
+$coverageFile = "$($coverageDir)*.xml"
 $coverageReportFolder = "$($currentDir)\\coverage-outputs\\report\\"
 reportgenerator -reports:$coverageFile -targetdir:$coverageReportFolder
 # start "$($coverageReportFolder)index.htm"
