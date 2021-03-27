9a501dfda7cdf03842fd105f66545f126a40886f
Luís Garcês <luis_garces@live.com.pt>
2021-03-27 12:16:22 +0000

    [FEAT] Added new rule priority assignement shortcuts.

--- src/Rules.Framework/RuleAddPriorityOption.cs
+++ src/Rules.Framework/RuleAddPriorityOption.cs
@@ -6,19 +6,46 @@ namespace Rules.Framework
     public class RuleAddPriorityOption
     {
         /// <summary>
-        /// Gets or sets the rule name to use when <code>PriorityOptions.AtRuleName</code> option is selected.
+        /// Creates a <see cref="RuleAddPriorityOption"/> setted to placed at bottom of priority.
         /// </summary>
-        /// <value>
-        /// At rule name option value.
-        /// </value>
+        public static RuleAddPriorityOption AtBottom => new RuleAddPriorityOption
+        {
+            AtRuleNameOptionValue = null,
+            PriorityOption = PriorityOptions.AtBottom
+        };
+
+        /// <summary>
+        /// Creates a <see cref="RuleAddPriorityOption"/> setted to placed at top of priority.
+        /// </summary>
+        public static RuleAddPriorityOption AtTop => new RuleAddPriorityOption
+        {
+            AtRuleNameOptionValue = null,
+            PriorityOption = PriorityOptions.AtTop
+        };
+
+        /// <summary>
+        /// Gets or sets the rule name to use when
+        /// <code>PriorityOptions.AtRuleName</code>
+        /// option is selected.
+        /// </summary>
+        /// <value>At rule name option value.</value>
         public string AtRuleNameOptionValue { get; set; }
 
         /// <summary>
         /// Gets or sets the priority option.
         /// </summary>
-        /// <value>
-        /// The priority option.
-        /// </value>
+        /// <value>The priority option.</value>
         public PriorityOptions PriorityOption { get; set; }
+
+        /// <summary>
+        /// Creates a <see cref="RuleAddPriorityOption"/> setted by rule name.
+        /// </summary>
+        /// <param name="ruleName">Name of the rule.</param>
+        /// <returns></returns>
+        public static RuleAddPriorityOption ByRuleName(string ruleName) => new RuleAddPriorityOption
+        {
+            AtRuleNameOptionValue = ruleName,
+            PriorityOption = PriorityOptions.AtRuleName
+        };
     }
 }
