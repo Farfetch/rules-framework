aa79aaec1b429db69601e51f2994e944983d5f16
Luís Garcês <luis.garces@farfetch.com>
2021-10-09 13:55:20 +0100

    test: change test ps script to execute tests additionally with rules.framework proj reference

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,15 +1,41 @@
+function FindRulesFrameworkPackageVersion ([string] $projectFilePath) {
+    $res = dotnet list $projectFilePath package | Select-String -Pattern '> Rules.Framework' -CaseSensitive -SimpleMatch
+    $res -match '[0-9]+\.[0-9]+\.[0-9]+(-beta)?'
+    return $matches[0]
+}
+
 $globalTools = dotnet tool list -g
 $reportGeneratorTool = $globalTools | Select-String -Pattern "dotnet-reportgenerator-globaltool"
 
 If (!$reportGeneratorTool) {
-    dotnet tool install --global dotnet-reportgenerator-globaltool
+    dotnet tool install dotnet-reportgenerator-globaltool --global --ignore-failed-sources
 }
 
 $reportTimestamp = [DateTime]::UtcNow.ToString("yyyyMMdd_hhmmss")
 $currentDir = (Get-Location).Path
 $coverageDir = "$currentDir\\coverage-outputs\\$reportTimestamp\\"
 
+# Run with current core "Rules.Framework" package version on other packages
+dotnet test .\rules-framework.sln -m:1
+
+# Keep original package versions
+# $inMemoryOriginalPackageVersion = FindRulesFrameworkPackageVersion('src\Rules.Framework.Providers.InMemory\Rules.Framework.Providers.InMemory.csproj')
+# $mongoDbOriginalPackageVersion = FindRulesFrameworkPackageVersion('src\Rules.Framework.Providers.MongoDb\Rules.Framework.Providers.MongoDb.csproj')
+
+# Use project reference to "Rules.Framework" now
+dotnet add src\Rules.Framework.Providers.InMemory\Rules.Framework.Providers.InMemory.csproj reference src\Rules.Framework\Rules.Framework.csproj
+dotnet add src\Rules.Framework.Providers.MongoDb\Rules.Framework.Providers.MongoDb.csproj reference src\Rules.Framework\Rules.Framework.csproj
+
+# Run again with project reference
 dotnet test .\rules-framework.sln --collect:"XPlat Code Coverage" --results-directory:"$coverageDir" -m:1
+
+# Remove project reference to "Rules.Framework"
+dotnet remove src\Rules.Framework.Providers.InMemory\Rules.Framework.Providers.InMemory.csproj reference src\Rules.Framework\Rules.Framework.csproj
+dotnet remove src\Rules.Framework.Providers.MongoDb\Rules.Framework.Providers.MongoDb.csproj reference src\Rules.Framework\Rules.Framework.csproj
+
+# Restore original package versions
+# dotnet add src\Rules.Framework.Providers.InMemory\Rules.Framework.Providers.InMemory.csproj package --version $inMemoryOriginalPackageVersion Rules.Framework
+# dotnet add src\Rules.Framework.Providers.MongoDb\Rules.Framework.Providers.MongoDb.csproj package --version $mongoDbOriginalPackageVersion Rules.Framework
 
 reportgenerator -reports:"$($coverageDir)*\\*.xml" -targetdir:"$($coverageDir)" -reporttypes:Cobertura
 
