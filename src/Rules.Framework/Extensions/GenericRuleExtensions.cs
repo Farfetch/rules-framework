ec07679119aeccef702c31ceb32fc55c4aea0c1a
Carlos Miranda <carlos.miranda@farfetch.com>
2022-10-28 14:28:40 +0100

    chore: GenericRuleExtensions

--- src/Rules.Framework/Extensions/GenericRuleExtensions.cs
+++ src/Rules.Framework/Extensions/GenericRuleExtensions.cs
@@ -7,7 +7,15 @@ namespace Rules.Framework.Extensions
     {
         public static GenericRule ToGenericRule<TContentType, TConditionType>(this Rule<TContentType, TConditionType> rule)
         {
-            return new GenericRule();
+            return new GenericRule
+            {
+                //RootCondition = new ConditionNode<ConditionType>(),
+                //ContentContainer = new ContentContainer<ContentType>(ContentType, rule.ContentContainer.GetContentAs<ContentType>()),
+                DateBegin = rule.DateBegin,
+                DateEnd = rule.DateEnd,
+                Name = rule.Name,
+                Priority = rule.Priority
+            };
         }
     }
 }
