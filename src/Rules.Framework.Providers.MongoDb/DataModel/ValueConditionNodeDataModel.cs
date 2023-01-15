548116d3f5867f08efd247b592f0ff37a4074608
Carlos Miranda <carlos.miranda@farfetch.com>
2023-01-15 16:59:28 +0000

    chore: revert some code

--- src/Rules.Framework.Providers.MongoDb/DataModel/ValueConditionNodeDataModel.cs
+++ src/Rules.Framework.Providers.MongoDb/DataModel/ValueConditionNodeDataModel.cs
@@ -12,12 +12,12 @@ namespace Rules.Framework.Providers.MongoDb.DataModel
         [BsonElement(Order = 2)]
         [BsonRepresentation(MongoDB.Bson.BsonType.String)]
         public DataTypes DataType { get; set; }
-
-        [BsonElement(Order = 4)]
-        public object Operand { get; set; }
-
+                
         [BsonElement(Order = 3)]
         [BsonRepresentation(MongoDB.Bson.BsonType.String)]
         public Operators Operator { get; set; }
+
+        [BsonElement(Order = 4)]
+        public object Operand { get; set; }
     }
 }
