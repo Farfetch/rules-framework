b2b2256af1d414436296e8592e8fe1eb29132da3
Carlos Miranda <carlos.miranda@farfetch.com>
2023-02-14 15:37:07 +0000

    fix: fixed the web ui filter rules by value (content) (#121)
    * fix: fixed the web ui filter rules by value (content)

--- src/Rules.Framework.WebUI/Dto/ContentTypeDto.cs
+++ src/Rules.Framework.WebUI/Dto/ContentTypeDto.cs
@@ -2,6 +2,7 @@ namespace Rules.Framework.WebUI.Dto
 {
     internal sealed class ContentTypeDto
     {
+        public int Index { get; internal set; }
         public int ActiveRulesCount { get; internal set; }
         public string Name { get; internal set; }
         public int RulesCount { get; internal set; }
