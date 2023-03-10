4c61edc5fb914ed6573441330ae2f3f4bdaa3ba0
João Romeiro <joao.romeiro@farfetch.com>
2023-03-10 13:05:32 +0000

    Deleted stuff

--- src/Rules.Framework/Providers.InMemory/DataModel/ComposedConditionNodeDataModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Rules.Framework.Providers.InMemory.DataModel
-{
-    using System.Collections.Generic;
-
-    internal sealed class ComposedConditionNodeDataModel<TConditionType> : ConditionNodeDataModel<TConditionType>
-    {
-        public IEnumerable<ConditionNodeDataModel<TConditionType>> ChildConditionNodes { get; set; }
-    }
-}
--- src/Rules.Framework/Providers.InMemory/DataModel/ConditionNodeDataModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Rules.Framework.Providers.InMemory.DataModel
-{
-    using Rules.Framework.Core;
-
-    internal class ConditionNodeDataModel<TConditionType>
-    {
-        public LogicalOperators LogicalOperator { get; set; }
-    }
-}
