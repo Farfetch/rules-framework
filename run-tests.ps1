f2bc4618d0d04d57a29402d5b7d1f4c7a339bc1f
Luis Garces <luis_garces@live.com.pt>
2023-04-01 12:25:46 +0100

    fix: remove logic of adding/removing references on local tests script

--- run-tests.ps1
+++ run-tests.ps1
@@ -11,29 +11,11 @@ $reportTimestamp = [DateTime]::UtcNow.ToString("yyyyMMdd_hhmmss")
 $currentDir = (Get-Location).Path
 $coverageDir = "$currentDir\\coverage-outputs\\$reportTimestamp\\"
 
-# Run with current core "Rules.Framework" package version on other packages
-if ($OnlyRunUnitTests) {
-    dotnet test .\rules-framework.sln -m:1 --filter 'Category=Unit'
-} else {
-    dotnet test .\rules-framework.sln -m:1
-}
-
-# Use project reference to "Rules.Framework" now
-dotnet add src\Rules.Framework.Providers.InMemory\Rules.Framework.Providers.InMemory.csproj reference src\Rules.Framework\Rules.Framework.csproj
-dotnet add src\Rules.Framework.Providers.MongoDb\Rules.Framework.Providers.MongoDb.csproj reference src\Rules.Framework\Rules.Framework.csproj
-dotnet add src\Rules.Framework.WebUI\Rules.Framework.WebUI.csproj reference src\Rules.Framework\Rules.Framework.csproj
-
-# Run again with project reference
 if ($OnlyRunUnitTests) {
     dotnet test .\rules-framework.sln --collect:"XPlat Code Coverage" --results-directory:"$coverageDir" -m:1 --filter 'Category=Unit'
 } else {
     dotnet test .\rules-framework.sln --collect:"XPlat Code Coverage" --results-directory:"$coverageDir" -m:1
 }
-
-# Remove project reference to "Rules.Framework"
-dotnet remove src\Rules.Framework.Providers.InMemory\Rules.Framework.Providers.InMemory.csproj reference src\Rules.Framework\Rules.Framework.csproj
-dotnet remove src\Rules.Framework.Providers.MongoDb\Rules.Framework.Providers.MongoDb.csproj reference src\Rules.Framework\Rules.Framework.csproj
-dotnet remove src\Rules.Framework.WebUI\Rules.Framework.WebUI.csproj reference src\Rules.Framework\Rules.Framework.csproj
 
 reportgenerator -reports:"$($coverageDir)*\\*.xml" -targetdir:"$($coverageDir)" -reporttypes:Cobertura
 
