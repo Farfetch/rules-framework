4a8ac23c56aa808105fd77a33f3928edd3267d5c
João Romeiro <joao.romeiro@farfetch.com>
2023-04-27 17:36:06 +0100

    Move docs to Wiki and modify README (#139)
    * updated readme.md 
    * move docs to wiki

---
+++ samples/Rules.Framework.WebUI.Sample/Engine/RuleSpecificationBase.cs
@@ -0,0 +1,20 @@
+namespace Rules.Framework.WebUI.Sample.Engine
+{
+    using global::Rules.Framework;
+    using global::Rules.Framework.Builder;
+
+    internal class RuleSpecificationBase<TContentType, TConditionType>
+    {
+        public RuleSpecificationBase(
+            RuleBuilderResult<TContentType, TConditionType> ruleBuilderResult,
+            RuleAddPriorityOption ruleAddPriorityOption)
+        {
+            this.RuleBuilderResult = ruleBuilderResult;
+            this.RuleAddPriorityOption = ruleAddPriorityOption;
+        }
+
+        public RuleAddPriorityOption RuleAddPriorityOption { get; set; }
+
+        public RuleBuilderResult<TContentType, TConditionType> RuleBuilderResult { get; set; }
+    }
+}
---
+++ samples/Rules.Framework.WebUI.Sample/ReadmeExample/BasicContentType.cs
@@ -0,0 +1,8 @@
+namespace Rules.Framework.WebUI.Sample.ReadmeExample
+{
+    public enum BasicContentType
+    {
+        FreeSample = 1,
+        ShippingCost = 2
+    }
+}
