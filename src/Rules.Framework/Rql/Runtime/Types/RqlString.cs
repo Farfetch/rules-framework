f3c97d0bdb5df3980c457ac92fcd5ae653741eff
Luis Garces <luis_garces@live.com.pt>
2023-06-28 19:04:07 +0100

    feat: add built-in property Size for strings

--- src/Rules.Framework/Rql/Runtime/Types/RqlString.cs
+++ src/Rules.Framework/Rql/Runtime/Types/RqlString.cs
@@ -1,9 +1,10 @@
 namespace Rules.Framework.Rql.Runtime.Types
 {
     using System;
+    using System.Collections.Generic;
     using Rules.Framework.Rql.Runtime;
 
-    public readonly struct RqlString : IRuntimeValue
+    public readonly struct RqlString : IRuntimeValue, IPropertyGet
     {
         private static readonly Type runtimeType = typeof(string);
         private static readonly RqlType type = RqlTypes.String;
@@ -27,7 +28,29 @@ namespace Rules.Framework.Rql.Runtime.Types
 
         public static implicit operator string(RqlString rqlString) => rqlString.Value;
 
+        public RqlAny GetPropertyValue(RqlString name)
+        {
+            if (this.TryGetPropertyValue(name, out var result))
+            {
+                return result;
+            }
+
+            throw new KeyNotFoundException($"Key with name '{name.Value}' has not been found.");
+        }
+
         public override string ToString()
-            => $"<{Type.Name}> {this.Value}";
+                    => $"<{Type.Name}> {this.Value}";
+
+        public RqlBool TryGetPropertyValue(RqlString memberName, out RqlAny result)
+        {
+            if (string.Equals(memberName, "Size", StringComparison.Ordinal))
+            {
+                result = new RqlInteger(this.Value?.Length ?? 0);
+                return true;
+            }
+
+            result = new RqlNothing();
+            return false;
+        }
     }
 }
