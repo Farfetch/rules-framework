1e75b9ed2b000dd01442e40ab3d022da2a00c60f
Luis Garces <luis_garces@live.com.pt>
2024-10-19 18:48:22 +0100

    refactor: extract export rules modal to separate component

--- src/Rules.Framework.WebUI/ViewModels/RuleViewModelExtensions.cs
+++ src/Rules.Framework.WebUI/ViewModels/RuleViewModelExtensions.cs
@@ -4,10 +4,74 @@ namespace Rules.Framework.WebUI.ViewModels
     using System.Collections.Generic;
     using System.Linq;
     using Rules.Framework.ConditionNodes;
+    using Rules.Framework.WebUI.Components.PageComponents;
     using Rules.Framework.WebUI.Services;
 
     internal static class RuleViewModelExtensions
     {
+        public static ExportRules.Rule ToExportRulesModel(this RuleViewModel ruleViewModel)
+        {
+            return new ExportRules.Rule
+            {
+                Active = ruleViewModel.Active,
+                Content = ruleViewModel.Content,
+                DateBegin = ruleViewModel.DateBegin,
+                DateEnd = ruleViewModel.DateEnd,
+                Name = ruleViewModel.Name,
+                Priority = ruleViewModel.Priority,
+                RootCondition = ruleViewModel.RootCondition.ToExportRulesModel(),
+                Ruleset = ruleViewModel.Ruleset,
+            };
+        }
+
+        public static ExportRules.ConditionNode ToExportRulesModel(this ConditionNodeViewModel rootCondition)
+        {
+            if (rootCondition.LogicalOperator == nameof(LogicalOperators.Eval))
+            {
+                var condition = rootCondition as ValueConditionNodeViewModel;
+
+                return new ExportRules.ValueConditionNode
+                {
+                    Condition = condition.Condition,
+                    DataType = condition.DataType.ToString(),
+                    LogicalOperator = condition.LogicalOperator,
+                    Operand = condition.Operand,
+                    Operator = condition.Operator.ToString(),
+                };
+            }
+
+            var composedConditionNode = rootCondition as ComposedConditionNodeViewModel;
+
+            var conditionNodeDataModels = new List<ExportRules.ConditionNode>(composedConditionNode.ChildConditionNodes.Count());
+
+            foreach (var child in composedConditionNode.ChildConditionNodes)
+            {
+                conditionNodeDataModels.Add(child.ToExportRulesModel());
+            }
+
+            return new ExportRules.ComposedConditionNode
+            {
+                ChildConditionNodes = conditionNodeDataModels,
+                LogicalOperator = composedConditionNode.LogicalOperator.ToString(),
+            };
+        }
+
+        public static RuleViewModel ToViewModel(this Rule rule)
+        {
+            return new RuleViewModel
+            {
+                Active = rule.Active,
+                Content = rule.ContentContainer.GetContentAs<object>(),
+                DateBegin = rule.DateBegin,
+                DateEnd = rule.DateEnd,
+                Id = GuidGenerator.GenerateFromString(rule.Name),
+                Name = rule.Name,
+                Priority = rule.Priority,
+                RootCondition = rule.RootCondition.ToViewModel(),
+                Ruleset = rule.Ruleset,
+            };
+        }
+
         public static ConditionNodeViewModel ToViewModel(this IConditionNode rootCondition)
         {
             if (rootCondition.LogicalOperator == LogicalOperators.Eval ||
@@ -19,6 +83,7 @@ namespace Rules.Framework.WebUI.ViewModels
                 {
                     Condition = condition.Condition,
                     DataType = condition.DataType.ToString(),
+                    LogicalOperator = condition.LogicalOperator.ToString(),
                     Operand = condition.Operand,
                     Operator = condition.Operator.ToString(),
                 };
@@ -37,22 +102,6 @@ namespace Rules.Framework.WebUI.ViewModels
             {
                 ChildConditionNodes = conditionNodeDataModels,
                 LogicalOperator = composedConditionNode.LogicalOperator.ToString()
-            };
-        }
-
-        public static RuleViewModel ToViewModel(this Rule rule)
-        {
-            return new RuleViewModel
-            {
-                Active = rule.Active,
-                Content = rule.ContentContainer.GetContentAs<object>(),
-                DateBegin = rule.DateBegin,
-                DateEnd = rule.DateEnd,
-                Id = GuidGenerator.GenerateFromString(rule.Name),
-                Name = rule.Name,
-                Priority = rule.Priority,
-                RootCondition = rule.RootCondition.ToViewModel(),
-                Ruleset = rule.Ruleset,
             };
         }
     }
