5ad149a92a586ac893d769e61ea4885865ca8220
Luis Garces <luis_garces@live.com.pt>
2024-10-19 15:05:33 +0100

    chore: feedback changes

---
+++ src/Rules.Framework.WebUI/WebUIConstants.cs
@@ -0,0 +1,9 @@
+namespace Rules.Framework.WebUI
+{
+    internal static class WebUIConstants
+    {
+        public const string IsUniqueInstanceStorageKey = "Is-Unique-Instance";
+        public const string SelectedInstanceStorageKey = "Selected-Instance-ID";
+        public const string SelectedRulesetsStorageKey = "Selected-Ruleset-IDs";
+    }
+}
